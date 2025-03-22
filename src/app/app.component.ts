a99231b44f69220acca896b0ade0f6de8003bb4c
Diksha <diksha.trivedi2122@gmail.com>
2025-03-22 12:11:51 +0530

    Resolved merge conflicts in app.component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
   message: string = "Hello from Diksha !";
+  logo: string = 'https://cdn.dribbble.com/users/246068/screenshots/15294543/hi_1_4x.gif?compress=1&resize=400x300';
+
+
 }
