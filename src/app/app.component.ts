a3c699f0fffbb152b3f30c6ce1694392c2829080
Diksha <diksha.trivedi2122@gmail.com>
2025-03-22 14:43:03 +0530

    By clicking logo, url open finctionality added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,8 @@ export class AppComponent {
 
   message: string = "Hello from Diksha !";
   logo: string = 'https://cdn.dribbble.com/users/246068/screenshots/15294543/hi_1_4x.gif?compress=1&resize=400x300';
+  url : string = 'https://a-z-animals.com/media/2023/04/shutterstock_716019169.jpg';
+  openLogo() : void {
+    window.open(this.url, '_blank');
+  }
 }
